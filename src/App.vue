ef5e9c34a066fe2bd9b63ad3180096087b59fbb4
sun <857153966@qq.com>
2021-01-01 16:39:15 +0800

    feat：steup函数

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{count}}</h1>
+  <h1>{{double}}</h1>
+  <button @click="increase">加加</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
+// import { defineComponent } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import { ref, computed } from 'vue'
+export default ({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const count = ref(0)
+    const double = computed(() => {
+      return count.value * 2
+    })
+    const increase = () => {
+      console.log(count)
+      count.value++
+    }
+    return {
+      count,
+      increase,
+      double
+    }
   }
 });
 </script>
